24eea135ec3fa393bae6978f0ca8918673041ffe
Andreas Pape <andreas.pape@manrental.eu>
2018-01-23 11:49:08 +0100

    extend Command and Event

--- core/src/main/scala/de/ioswarm/hyperion/model/Command.scala
+++ core/src/main/scala/de/ioswarm/hyperion/model/Command.scala
@@ -3,3 +3,5 @@ package de.ioswarm.hyperion.model
 trait Command {
   def id: String
 }
+
+final case class Get(id: String) extends Command
--- core/src/main/scala/de/ioswarm/hyperion/model/Event.scala
+++ core/src/main/scala/de/ioswarm/hyperion/model/Event.scala
@@ -3,3 +3,9 @@ package de.ioswarm.hyperion.model
 trait Event {
 
 }
+
+final case class Result(result: Any) extends Event {
+
+  def as[T]: T = result.asInstanceOf[T]
+
+}
