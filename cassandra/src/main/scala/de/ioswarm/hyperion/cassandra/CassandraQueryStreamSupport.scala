317e7a57edcaa7e285fce019f89dddbbb35ba8ca
Andreas Pape <andreas.pape@manrental.eu>
2018-01-31 17:06:02 +0100

    Cassandra QueryStreams Impl

--- cassandra/src/main/scala/de/ioswarm/hyperion/cassandra/CassandraQueryStreamSupport.scala
+++ cassandra/src/main/scala/de/ioswarm/hyperion/cassandra/CassandraQueryStreamSupport.scala
@@ -11,14 +11,16 @@ object CassandraQueryStreamSupport {
 
 
 
-  def eventsByPersistenceId(sink: Sink[EventEnvelope, NotUsed]): QueryStream  = new CassandraQueryStream(sink) {
+  def eventsByPersistenceId(f: ActorSystem => Sink[EventEnvelope, NotUsed]): QueryStream  = new CassandraQueryStream(f) {
 
     override def source(id: String, fromSeq: Long, toSeq: Long, system: ActorSystem): Source[EventEnvelope, NotUsed] = journal(system).eventsByPersistenceId(id, fromSeq, toSeq)
 
   }
 
 
-  sealed abstract class CassandraQueryStream(val sink: Sink[EventEnvelope, NotUsed]) extends QueryStream {
+  sealed abstract class CassandraQueryStream(f: ActorSystem => Sink[EventEnvelope, NotUsed]) extends QueryStream {
+
+    def sink(system: ActorSystem): Sink[EventEnvelope, NotUsed] = f(system)
 
     def journal(system: ActorSystem): CassandraReadJournal = PersistenceQuery(system).readJournalFor[CassandraReadJournal](CassandraReadJournal.Identifier)
 
