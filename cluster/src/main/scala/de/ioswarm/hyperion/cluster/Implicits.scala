7e2cdcad6762392da644219bbd362466428cff68
Andreas Pape <andreas@ioswarm.de>
2019-11-24 23:41:04 +0100

    extend with ServiceBuilderImplicits

--- cluster/src/main/scala/de/ioswarm/hyperion/cluster/Implicits.scala
+++ cluster/src/main/scala/de/ioswarm/hyperion/cluster/Implicits.scala
@@ -1,6 +1,7 @@
 package de.ioswarm.hyperion.cluster
 
-import de.ioswarm.hyperion.CoreImplicits
+import de.ioswarm.hyperion.{CoreImplicits, ServiceBuilderImplicits}
 
 object Implicits extends CoreImplicits
   with ClusterImplicits
+  with ServiceBuilderImplicits
